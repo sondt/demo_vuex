a80172ffc5190c503200136475d90baa2fbd43c5
sondt <sondt.vn@gmail.com>
2022-09-13 12:03:59 +0700

    :tada: Add state

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    name: 'John Doe',
   },
   getters: {
+    getName: state => {
+      return state.name
+    }
   },
   mutations: {
+    setName: (state, payload) => {
+      state.name = payload
+    }
   },
   actions: {
+    setName: ({ commit }, payload) => {
+      commit('setName', payload)
+    }
+  },
+  dispatch: {
+    setName: ({ commit }, payload) => {
+      commit('setName', payload)
+    }
   },
   modules: {
+
   }
 })
---
+++ src/views/StateView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="state">
+    <h1>State</h1>
+    <p>Name: {{ $store.getters.getName }}</p>
+    <p>
+      <input type="text" name="name" id="name" placeholder="Enter name">
+    </p>
+    <p>
+      <button @click="updateName">Update Name</button>
+    </p>
+  </div>
+</template>
+
+<script>
+import store from "@/store";
+import { mapState } from 'vuex'
+
+export default {
+  name: "StateView",
+  computed: mapState({
+    // arrow functions can make the code very succinct!
+    myName: store.state.name,
+  }),
+  methods: {
+    updateName() {
+      //this.$store.commit("setName", document.getElementById("name").value);
+      store.state.name = document.getElementById("name").value;
+
+    },
+  },
+}
+
+</script>
+
+<style scoped>
+
+</style>
